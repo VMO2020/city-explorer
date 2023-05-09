2571fbe907f2862afdbdecb84183f637460574d0
VICTOR M OTTATI <victor.ottati@gmail.com>
2023-05-09 14:48:43 +0100

    UPDATE: App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const token = process.env.REACT_APP_ACCESS_TOKEN;
-
 function App() {
 	const [searchQuery, setSearchQuery] = useState({});
 	const [location, setLocation] = useState({});
@@ -16,7 +14,9 @@ function App() {
 	const getLocation = async () => {
 		try {
 			const data = await axios.get(
-				`https://eu1.locationiq.com/v1/search?key=${token}&q=${searchQuery.toLowerCase()}&format=json`
+				`https://eu1.locationiq.com/v1/search?key=${
+					process.env.REACT_APP_ACCESS_TOKEN
+				}&q=${searchQuery.toLowerCase()}&format=json`
 			);
 			// console.log(data.data[0]);
 			setLocation(data.data[0]);
