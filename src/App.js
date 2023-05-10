c9059227574b7044793a6c5a7f1deef045ec3123
VICTOR M OTTATI <victor.ottati@gmail.com>
2023-05-10 16:17:09 +0100

    UPDATE: Add map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import axios from 'axios';
 import './App.css';
+// import city from './images/city.png';
 
 function App() {
 	const [searchQuery, setSearchQuery] = useState('');
 	const [location, setLocation] = useState({});
+	const [locationMap, setLocationMap] = useState('');
 	const [reset, setReset] = useState(false);
 
 	const handleChange = (event) => {
@@ -15,21 +17,39 @@ function App() {
 	const handleReset = () => {
 		setSearchQuery('');
 		setLocation({});
+		setLocationMap('');
 		setReset(false);
 	};
 
 	const getLocation = async () => {
 		try {
-			const data = await axios.get(
-				`https://eu1.locationiq.com/v1/search?key=${
-					process.env.REACT_APP_ACCESS_TOKEN
-				}&q=${searchQuery.toLowerCase()}&format=json`
-			);
+			const API = `https://eu1.locationiq.com/v1/search?key=${
+				process.env.REACT_APP_ACCESS_TOKEN
+			}&q=${searchQuery.toLowerCase()}&format=json`;
+			const data = await axios.get(API);
 			// console.log(data.data[0]);
 			setLocation(data.data[0]);
+			getMap(data);
 			setReset(true);
 		} catch (error) {
-			console.log(error);
+			console.error(error.message);
+			setReset(true);
+		}
+	};
+
+	const getMap = async (data) => {
+		try {
+			// console.log(data.data[0]);
+			let lat = data.data[0].lat;
+			let lon = data.data[0].lon;
+			// console.log(lat, lon);
+
+			const API2 = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_ACCESS_TOKEN}&center=${lat},${lon}&size=600x600&zoom=12&path=fillcolor:%2390EE90|weight:2|color:blue|17.452945,78.380055|17.452765,78.382026|17.452020,78.381375|17.452045,78.380846|17.452945,78.380055`;
+			const map = await axios.get(API2);
+			// console.log(map.config.url);
+			setLocationMap(map.config.url);
+		} catch (error) {
+			console.error(error);
 		}
 	};
 
@@ -60,6 +80,7 @@ function App() {
 					</p>
 				</div>
 			)}
+			{locationMap && <img src={locationMap} alt="map" />}
 		</div>
 	);
 }
